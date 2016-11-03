ed0443aeed08da6a9dadfc4613fe0fe1dfdae602
osanchez <osanchez@d0c2015e-0c26-44a3-b760-eac3cb1bf761>
2016-11-03 22:54:44 +0000

    [sivad] * Se agregan carpetas para los microservicios de Siva Diamantes

---
+++ src/main/java/mx/com/nmp/ms/sivad/referencia/TablasReferenciaApplication.java
@@ -0,0 +1,12 @@
+package mx.com.nmp.ms.sivad.referencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablasReferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablasReferenciaApplication.class, args);
+	}
+}
---
+++ src/test/java/mx/com/nmp/ms/sivad/referencia/TablasReferenciaApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.com.nmp.ms.sivad.referencia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TablasReferenciaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
