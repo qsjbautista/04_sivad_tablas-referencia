99d4cdebcca09f5166edebc452b619c264a83a84
jbautista <jbautista@d0c2015e-0c26-44a3-b760-eac3cb1bf761>
2016-12-02 19:03:26 +0000

    Se actualizan clases de Tablas de referencia Alhajas y Diamantes que se solicitan información atravez de servicios.

--- src/main/java/mx/com/nmp/ms/sivad/referencia/api/ws/ReferenciaAlhajaServiceEndpoint.java
+++ src/main/java/mx/com/nmp/ms/sivad/referencia/api/ws/ReferenciaAlhajaServiceEndpoint.java
@@ -1,37 +1,78 @@
 package mx.com.nmp.ms.sivad.referencia.api.ws;
 
 
+import mx.com.nmp.ms.sivad.referencia.adminapi.exception.WebServiceExceptionFactory;
+import mx.com.nmp.ms.sivad.referencia.dominio.exception.FactorAlhajaNoEncontradoException;
+import mx.com.nmp.ms.sivad.referencia.dominio.exception.ValorGramoNoEncontradoException;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.FactorAlhaja;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.Metal;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.Oro;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.vo.FactorAlhajaVO;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.vo.MetalVO;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.vo.OroVO;
+import mx.com.nmp.ms.sivad.referencia.dominio.repository.ModificadorRangoRepository;
+import mx.com.nmp.ms.sivad.referencia.dominio.repository.ValorComercialMetalRepository;
+import mx.com.nmp.ms.sivad.referencia.dominio.repository.ValorComercialOroRepository;
 import mx.com.nmp.ms.sivad.referencia.ws.alhajas.ReferenciaAlhajaService;
 import mx.com.nmp.ms.sivad.referencia.ws.alhajas.datatypes.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
-import java.math.BigDecimal;
+import javax.inject.Inject;
 
+/**
+ * Clase que contiene los metodos para recuperar información necesaria de diamantes.
+ *
+ * @author jbautista
+ */
 public class ReferenciaAlhajaServiceEndpoint implements ReferenciaAlhajaService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReferenciaAlhajaServiceEndpoint.class);
 
+    @Inject
+    ValorComercialOroRepository valorComercialOroRepository;
+
+    @Inject
+    ValorComercialMetalRepository valorComercialMetalRepository;
+
+    @Inject
+    ModificadorRangoRepository modificadorRangoRepository;
+
     /**
-     * @param parameters
+     * Obtiene el valor por gramo correspondiente a las caracteristicas del metal de una alhaja.
+     *
+     * @param parameters color y calidad
      * @return returns mx.com.nmp.ms.ws.referencia.datatypes.GetOroResponse
      */
     @Override
     public ObtenerValorGramoOroResponse obtenerValorGramoOro(ObtenerValorGramoOroRequest parameters) {
         LOGGER.info(">> obtenerValorGramoOro({})", parameters);
 
+        ObtenerValorGramoOroResponse response = new ObtenerValorGramoOroResponse();
+
         if (parameters != null) {
-            LOGGER.info("'{}','{}'", parameters.getColor(), parameters.getCalidad());
+            try {
+                OroVO oroVO = new OroVO(parameters.getColor(), Integer.parseInt(parameters.getCalidad()));
+                Oro oroResult = valorComercialOroRepository.consultarOroVigente(oroVO);
+                response.setPrecioPorGramo(oroResult.obtenerValorGramo());
+            } catch (ValorGramoNoEncontradoException e) {
+                LOGGER.info(e.getMessage());
+                throw WebServiceExceptionFactory.crearWebServiceExceptionCon("NMP-TR-008", e.getMessage());
+            }
         }
 
-        ObtenerValorGramoOroResponse response = new ObtenerValorGramoOroResponse();
-        response.setPrecioPorGramo(BigDecimal.TEN);
         LOGGER.info("<< {}", response.getPrecioPorGramo());
+
         return response;
     }
 
     /**
-     * @param parameters
+     * Permite obtener el valor por gramo de metales distintos a oro.
+     *
+     * @param parameters Metal y Calidad
      * @return returns mx.com.nmp.ms.ws.referencia.datatypes.ObtenerValorGramoMetalResponse
      */
     @Override
@@ -39,13 +80,26 @@ public class ReferenciaAlhajaServiceEndpoint implements ReferenciaAlhajaService 
         LOGGER.info(">> obtenerValorGramoMetal({})", parameters);
 
         ObtenerValorGramoMetalResponse response = new ObtenerValorGramoMetalResponse();
-        response.setPrecioPorGramo(BigDecimal.TEN);
 
+        if (parameters != null) {
+            try {
+                MetalVO metalVO = new MetalVO(parameters.getMetal(), parameters.getCalidad());
+                Metal metal = valorComercialMetalRepository.consultarMetalVigente(metalVO);
+                response.setPrecioPorGramo(metal.obtenerValorGramo());
+            } catch (ValorGramoNoEncontradoException e) {
+                LOGGER.info(e.getMessage());
+                throw WebServiceExceptionFactory.crearWebServiceExceptionCon("NMP-TR-011", e.getMessage());
+            }
+        }
+
+        LOGGER.info("<< {}", response.getPrecioPorGramo());
         return response;
     }
 
     /**
-     * @param parameters
+     * Obtiene el limite maximo correspondiente al modificador por desplazamiento comercial de la alhaja.
+     *
+     * @param parameters Metal, Calidad y Rango.
      * @return returns mx.com.nmp.ms.ws.referencia.datatypes.ObtenerDesplazamientoResponse
      */
     @Override
@@ -53,13 +107,26 @@ public class ReferenciaAlhajaServiceEndpoint implements ReferenciaAlhajaService 
         LOGGER.info(">> obtenerDesplazamiento({})", parameters);
 
         ObtenerDesplazamientoResponse response = new ObtenerDesplazamientoResponse();
-        response.setDesplazamiento(BigDecimal.ONE);
+
+        if (parameters != null) {
+            try {
+                FactorAlhajaVO factorAlhajaVO = new FactorAlhajaVO(parameters.getMetal(), parameters.getCalidad(), parameters.getRango());
+                FactorAlhaja factorAlhaja = modificadorRangoRepository.obtenerFactor(factorAlhajaVO);
+                response.setDesplazamiento(factorAlhaja.getFactor());
+            } catch (FactorAlhajaNoEncontradoException e) {
+                LOGGER.info("No existe el rango solicitado");
+                throw WebServiceExceptionFactory.crearWebServiceExceptionCon("NPM-TR-007", "No existe el rango solicitado");
+            }
+        }
+
 
         return response;
     }
 
     /**
-     * @param parameters
+     * Obtiene el factor de modificación aplicable a la relación metal-calida-rango de una alhaja.
+     *
+     * @param parameters Metal, Calidad y Rango
      * @return returns mx.com.nmp.ms.sivad.referencia.ws.datatypes.ObtenerFactorResponse
      */
     @Override
@@ -67,13 +134,25 @@ public class ReferenciaAlhajaServiceEndpoint implements ReferenciaAlhajaService 
         LOGGER.info(">> obtenerDesplazamiento({})", parameters);
 
         ObtenerFactorResponse response = new ObtenerFactorResponse();
-        response.setFactor(BigDecimal.TEN);
+
+        if (parameters != null) {
+            try {
+                FactorAlhajaVO factorAlhajaVO = new FactorAlhajaVO(parameters.getMetal(), parameters.getCalidad(), parameters.getRango());
+                FactorAlhaja factorAlhaja = modificadorRangoRepository.obtenerFactor(factorAlhajaVO);
+                response.setFactor(factorAlhaja.getFactor());
+            } catch (FactorAlhajaNoEncontradoException e) {
+                LOGGER.info("No existe el rango solicitado");
+                throw WebServiceExceptionFactory.crearWebServiceExceptionCon("NPM-TR-007", "No existe el rango solicitado");
+            }
+        }
 
         return response;
     }
 
     /**
-     * @param parameters
+     * Obtiene el rango de incremento asociado a la relación metal-calidad-rango.
+     *
+     * @param parameters Metal, Calidad y Rango
      * @return returns mx.com.nmp.ms.sivad.referencia.ws.datatypes.ObtenerLimitesIncrementoResponse
      */
     @Override
@@ -81,10 +160,23 @@ public class ReferenciaAlhajaServiceEndpoint implements ReferenciaAlhajaService 
         LOGGER.info(">> obtenerDesplazamiento({})", parameters);
 
         ObtenerLimitesIncrementoResponse response = new ObtenerLimitesIncrementoResponse();
-        RangoLimite rangoLimite = new RangoLimite();
-        rangoLimite.setLimiteInferior(BigDecimal.ONE);
-        rangoLimite.setLimiteSuperior(BigDecimal.TEN);
-        response.setLimitesIncremento(rangoLimite);
+
+        if (parameters != null) {
+            try {
+                FactorAlhajaVO factorAlhajaVO = new FactorAlhajaVO(parameters.getMetal(), parameters.getCalidad(), parameters.getRango());
+                FactorAlhaja factorAlhaja = modificadorRangoRepository.obtenerFactor(factorAlhajaVO);
+
+                RangoLimite rangoLimite = new RangoLimite();
+                rangoLimite.setLimiteInferior(factorAlhaja.getLimiteInferior());
+                rangoLimite.setLimiteSuperior(factorAlhaja.getLimiteSuperior());
+
+                response.setLimitesIncremento(rangoLimite);
+
+            } catch (FactorAlhajaNoEncontradoException e) {
+                LOGGER.info("No existe el rango solicitado");
+                throw WebServiceExceptionFactory.crearWebServiceExceptionCon("NPM-TR-007", "No existe el rango solicitado");
+            }
+        }
 
         return response;
     }
--- src/main/java/mx/com/nmp/ms/sivad/referencia/api/ws/ReferenciaDiamantesServiceEndpoint.java
+++ src/main/java/mx/com/nmp/ms/sivad/referencia/api/ws/ReferenciaDiamantesServiceEndpoint.java
@@ -1,19 +1,39 @@
 package mx.com.nmp.ms.sivad.referencia.api.ws;
 
+import mx.com.nmp.ms.sivad.referencia.adminapi.exception.WebServiceExceptionFactory;
+import mx.com.nmp.ms.sivad.referencia.dominio.exception.CertificadoNoEncontradoException;
+import mx.com.nmp.ms.sivad.referencia.dominio.exception.ValorComercialNoEncontradoException;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.Certificado;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.Diamante;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.vo.CertificadoVO;
+import mx.com.nmp.ms.sivad.referencia.dominio.modelo.vo.DiamanteVO;
+import mx.com.nmp.ms.sivad.referencia.dominio.repository.ModificadorCertificadoRepository;
+import mx.com.nmp.ms.sivad.referencia.dominio.repository.ValorComercialDiamanteRepository;
 import mx.com.nmp.ms.sivad.referencia.ws.diamantes.ReferenciaDiamanteService;
 import mx.com.nmp.ms.sivad.referencia.ws.diamantes.datatypes.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.math.BigDecimal;
+import javax.inject.Inject;
 
 /**
+ * Clase que contiene los metodos que son invocados para obtener la información comercial de diamantes.
+ *
  * @author osanchez
  */
 public class ReferenciaDiamantesServiceEndpoint implements ReferenciaDiamanteService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ReferenciaDiamantesServiceEndpoint.class);
 
+    @Inject
+    ValorComercialDiamanteRepository valorComercialDiamanteRepository;
+
+    @Inject
+    ModificadorCertificadoRepository modificadorCertificadoRepository;
+
+
     /**
+     * Obtiene el valor comercial de diamante en base a sus caracteristcas.
+     *
      * @param parameters
      * @return returns mx.com.nmp.ms.sivad.referencia.ws.diamantes.datatypes.ObtenerValorComercialResponse
      */
@@ -22,16 +42,31 @@ public class ReferenciaDiamantesServiceEndpoint implements ReferenciaDiamanteSer
         LOGGER.info(">> obtenerValorComercial({})", parameters);
 
         ObtenerValorComercialResponse response = new ObtenerValorComercialResponse();
-        ValorComercial valorComercial = new ValorComercial();
-        valorComercial.setValorMinimo(BigDecimal.ZERO);
-        valorComercial.setValorMedio(BigDecimal.ONE);
-        valorComercial.setValorMaximo(BigDecimal.TEN);
-        response.setValorComercial(valorComercial);
+
+        if (parameters != null) {
+            DiamanteVO diamanteVO = new DiamanteVO(parameters.getCorte(), parameters.getColor(), parameters.getClaridad(), parameters.getQuilatesCt());
+
+            try {
+                Diamante diamante = valorComercialDiamanteRepository.obtenerValorComercial(diamanteVO);
+                ValorComercial valorComercial = new ValorComercial();
+
+                valorComercial.setValorMaximo(diamante.getTamanioSuperior());
+                valorComercial.setValorMedio(diamante.getPrecio());
+                valorComercial.setValorMinimo(diamante.getTamanioInferior());
+
+                response.setValorComercial(valorComercial);
+            } catch (ValorComercialNoEncontradoException e) {
+                LOGGER.info(e.getMessage());
+                throw WebServiceExceptionFactory.crearWebServiceExceptionCon("NPM-TR-010", e.getMessage());
+            }
+        }
 
         return response;
     }
 
     /**
+     * Obtiene el porcentaje de modificación de valor correspondiente a un certificado.
+     *
      * @param parameters
      * @return returns mx.com.nmp.ms.sivad.referencia.ws.diamantes.datatypes.ObtenerModificadorResponse
      */
@@ -40,8 +75,18 @@ public class ReferenciaDiamantesServiceEndpoint implements ReferenciaDiamanteSer
         LOGGER.info(">> obtenerModificador({})", parameters);
 
         ObtenerModificadorResponse response = new ObtenerModificadorResponse();
-        response.setFactor(BigDecimal.TEN);
 
+        if (parameters != null) {
+            CertificadoVO certificadoVO = new CertificadoVO(parameters.getCertificadoDiamante());
+
+            try {
+                Certificado factor = modificadorCertificadoRepository.consultarModificadorCertificadoVigente(certificadoVO);
+                response.setFactor(factor.getFactor());
+            } catch (CertificadoNoEncontradoException e) {
+                LOGGER.info(e.getMessage());
+                throw WebServiceExceptionFactory.crearWebServiceExceptionCon("NPM-TR-009", e.getMessage());
+            }
+        }
         return response;
     }
 }
