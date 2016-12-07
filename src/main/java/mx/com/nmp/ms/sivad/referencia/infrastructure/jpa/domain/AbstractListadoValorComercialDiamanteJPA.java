15b5b86c442f19d34455018f8eb5c3fa209146a4
ngonzalez <ngonzalez@d0c2015e-0c26-44a3-b760-eac3cb1bf761>
2016-12-07 00:38:27 +0000

    Se agregan encabezados faltantes.

--- src/main/java/mx/com/nmp/ms/sivad/referencia/infrastructure/jpa/domain/AbstractListadoValorComercialDiamanteJPA.java
+++ src/main/java/mx/com/nmp/ms/sivad/referencia/infrastructure/jpa/domain/AbstractListadoValorComercialDiamanteJPA.java
@@ -1,3 +1,7 @@
+/**
+ * Proyecto:        NMP - Microservicio de Tablas de Referencia
+ * Quarksoft S.A.P.I. de C.V. – Todos los derechos reservados. Para uso exclusivo de Nacional Monte de Piedad.
+ */
 package mx.com.nmp.ms.sivad.referencia.infrastructure.jpa.domain;
 
 import org.hibernate.annotations.Type;
