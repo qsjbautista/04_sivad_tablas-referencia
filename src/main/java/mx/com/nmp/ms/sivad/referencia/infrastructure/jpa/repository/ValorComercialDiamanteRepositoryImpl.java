ec55e5a4f9fcde42edec7af9b149049f08cbff98
ngonzalez <ngonzalez@d0c2015e-0c26-44a3-b760-eac3cb1bf761>
2016-12-26 23:41:31 +0000

    La lógica de la validación del listado (Diamante) choca con la lógica del proceso batch. Por tal motivo esa validación se elimina.

--- src/main/java/mx/com/nmp/ms/sivad/referencia/infrastructure/jpa/repository/ValorComercialDiamanteRepositoryImpl.java
+++ src/main/java/mx/com/nmp/ms/sivad/referencia/infrastructure/jpa/repository/ValorComercialDiamanteRepositoryImpl.java
@@ -9,7 +9,6 @@ import mx.com.nmp.ms.sivad.referencia.conector.Convertidor;
 import mx.com.nmp.ms.sivad.referencia.conector.TipoCambioEnum;
 import mx.com.nmp.ms.sivad.referencia.dominio.exception.FechaVigenciaFuturaException;
 import mx.com.nmp.ms.sivad.referencia.dominio.exception.ListadoValorComercialNoEncontradoException;
-import mx.com.nmp.ms.sivad.referencia.dominio.exception.ListadoValorComercialSinElementosException;
 import mx.com.nmp.ms.sivad.referencia.dominio.exception.ValorComercialNoEncontradoException;
 import mx.com.nmp.ms.sivad.referencia.dominio.modelo.Diamante;
 import mx.com.nmp.ms.sivad.referencia.dominio.modelo.DiamanteFactory;
@@ -179,13 +178,6 @@ public class ValorComercialDiamanteRepositoryImpl implements ValorComercialDiama
     @Transactional
     public void actualizarListado(@NotNull ListadoValorComercialDiamante listado) {
         LOGGER.info(">> actualizarListado({})", listado);
-
-        if (ObjectUtils.isEmpty(listado) ||
-            ObjectUtils.isEmpty(listado.getValoresComerciales())) {
-            String msg = "El nuevo listado no contiene elementos.";
-            LOGGER.error(msg);
-            throw new ListadoValorComercialSinElementosException(msg, ListadoValorComercialDiamanteJPA.class);
-        }
 
         ListadoValorComercialDiamanteJPA listadoVigente = listadoJpaRepository.obtenerListadoVigente();
 
